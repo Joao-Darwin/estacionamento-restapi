247d4f4597da6c83d999f7ad2f15aaf29a4e7e19
João Darwin <joaodarwin.ip22@gmail.com>
2023-09-24 22:38:38 -0300

    feat: Criar repository para Company

---
+++ src/main/java/com/estacionamento/app/repositories/CompanyRepository.java
@@ -0,0 +1,9 @@
+package com.estacionamento.app.repositories;
+
+import com.estacionamento.app.entities.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+}
