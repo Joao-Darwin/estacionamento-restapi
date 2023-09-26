4f97c88b6576ebd475010b8fe7f1990bb7cced20
João Darwin <joaodarwin.ip22@gmail.com>
2023-09-26 14:45:17 -0300

    feat: Adicionar annotacion de não permissão de valores null para Vehicle

--- src/main/java/com/estacionamento/app/entities/Vehicle.java
+++ src/main/java/com/estacionamento/app/entities/Vehicle.java
@@ -12,10 +12,15 @@ public class Vehicle implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String brand;
+    @Column(nullable = false)
     private String model;
+    @Column(nullable = false)
     private String color;
+    @Column(nullable = false)
     private String plate;
+    @Column(nullable = false)
     private VehiclesType type;
 
     @ManyToOne
