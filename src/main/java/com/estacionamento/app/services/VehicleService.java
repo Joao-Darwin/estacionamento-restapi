01521c1ae32c925b83795b4e04b7363a093cdfcd
João Darwin <joaodarwin.ip22@gmail.com>
2023-09-27 20:35:03 -0300

    refactor: Refatorar método na camada de service para salvar veículo

--- src/main/java/com/estacionamento/app/services/VehicleService.java
+++ src/main/java/com/estacionamento/app/services/VehicleService.java
@@ -1,11 +1,15 @@
 package com.estacionamento.app.services;
 
 import com.estacionamento.app.entities.Vehicle;
+import com.estacionamento.app.exceptions.NotFoundException;
+import com.estacionamento.app.exceptions.NotSaveException;
 import com.estacionamento.app.repositories.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class VehicleService {
@@ -13,9 +17,13 @@ public class VehicleService {
     @Autowired
     private VehicleRepository vehicleRepository;
 
-    public Vehicle saveVehicle(Vehicle vehicle) throws Exception{
-        vehicleRepository.save(vehicle);
-        return vehicle;
+    public Vehicle saveVehicle(Vehicle vehicle) {
+        try {
+            vehicleRepository.save(vehicle);
+            return vehicle;
+        } catch (DataIntegrityViolationException exception) {
+            throw new NotSaveException("Vehicle does not save, vehicle already exists");
+        }
     }
 
     public List<Vehicle> findAll() {
