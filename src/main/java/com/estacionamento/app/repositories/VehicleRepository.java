b2ba1c5c6ff17472b87a4a7b2c003565e6e42141
João Darwin <joaodarwin.ip22@gmail.com>
2023-09-30 13:35:41 -0300

    refactor: Refatorar método para nova cosulta mais simplificada

--- src/main/java/com/estacionamento/app/repositories/VehicleRepository.java
+++ src/main/java/com/estacionamento/app/repositories/VehicleRepository.java
@@ -2,13 +2,9 @@ package com.estacionamento.app.repositories;
 
 import com.estacionamento.app.entities.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
-
-    @Query(value = "SELECT * FROM VEHICLE WHERE PLATE = :plate", nativeQuery = true)
-    Vehicle findByPlate(@Param("plate") String plate);
+    Vehicle findByPlate(String plate);
 }
