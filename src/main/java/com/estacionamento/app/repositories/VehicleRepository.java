aeed12693f70c49763bff51fcf88cfd6787b07d5
João Darwin <joaodarwin.ip22@gmail.com>
2023-09-30 13:34:44 -0300

    Exemplo de método que fazendo consulta SQL nativa

--- src/main/java/com/estacionamento/app/repositories/VehicleRepository.java
+++ src/main/java/com/estacionamento/app/repositories/VehicleRepository.java
@@ -2,8 +2,13 @@ package com.estacionamento.app.repositories;
 
 import com.estacionamento.app.entities.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+
+    @Query(value = "SELECT * FROM VEHICLE WHERE PLATE = :plate", nativeQuery = true)
+    Vehicle findByPlate(@Param("plate") String plate);
 }
