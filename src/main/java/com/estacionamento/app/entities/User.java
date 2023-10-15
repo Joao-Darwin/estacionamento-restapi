55ce75135380a3f07486abf779d16f7142bc6468
João Darwin <joaodarwin.ip22@gmail.com>
2023-10-15 20:57:23 -0300

    refactor: Implementar lombok na entity User

--- src/main/java/com/estacionamento/app/entities/User.java
+++ src/main/java/com/estacionamento/app/entities/User.java
@@ -3,9 +3,12 @@ package com.estacionamento.app.entities;
 import com.estacionamento.app.entities.enums.UserJobRole;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
 
+@Getter
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
@@ -13,57 +16,15 @@ public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Setter
     private String name;
+    @Setter
     @Column(unique = true, nullable = false)
     private String email;
+    @Setter
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
+    @Setter
     private UserJobRole role;
-
-    public User() {
-    }
-
-    public User(String name, String email, String password, UserJobRole role) {
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.role = role;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public UserJobRole getRole() {
-        return role;
-    }
-
-    public void setRole(UserJobRole role) {
-        this.role = role;
-    }
 }
