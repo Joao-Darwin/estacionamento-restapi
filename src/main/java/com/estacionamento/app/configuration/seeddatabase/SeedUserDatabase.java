837ce5c39b5809a83279190479799bc70c6ed2c0
João Darwin <joaodarwin.ip22@gmail.com>
2023-10-21 12:46:45 -0300

    feat: Criar seed de User automático no banco

---
+++ src/main/java/com/estacionamento/app/configuration/seeddatabase/SeedUserDatabase.java
@@ -0,0 +1,34 @@
+package com.estacionamento.app.configuration.seeddatabase;
+
+import com.estacionamento.app.entities.User;
+import com.estacionamento.app.entities.enums.UserJobRole;
+import com.estacionamento.app.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SeedUserDatabase implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public SeedUserDatabase(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void run(String... args) {
+        User user = new User();
+
+        user.setName("Admin");
+        user.setEmail("admin@admin.com");
+        user.setPassword(passwordEncoder.encode("admin@123"));
+        user.setRole(UserJobRole.ADMIN);
+
+        userRepository.save(user);
+    }
+}
