ebe65cd80433387a51133f53aae747c87cf8b9da
João Darwin <joaodarwin.ip22@gmail.com>
2023-10-28 19:26:31 -0300

    feat: Adicionar documentação para resources do User

--- src/main/java/com/estacionamento/app/resources/users/IUserResource.java
+++ src/main/java/com/estacionamento/app/resources/users/IUserResource.java
@@ -1,15 +1,49 @@
 package com.estacionamento.app.resources.users;
 
 import com.estacionamento.app.entities.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
+@Tag(name = "Users", description = "Users Resources")
 public interface IUserResource {
 
-    ResponseEntity<?> save(User user);
+    @Operation(summary = "Create user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User created!"),
+            @ApiResponse(responseCode = "501", description = "Not implemented", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
+    ResponseEntity<?> save(@RequestBody User user);
 
+    @Operation(summary = "Find all users")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success!"),
+            @ApiResponse(responseCode = "401", description = "Don't Authorization", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
     ResponseEntity<?> findAll();
 
-    ResponseEntity<?> updateUser(Long id, User userUpdated);
+    @Operation(summary = "Update User")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success!"),
+            @ApiResponse(responseCode = "404", description = "User don't found"),
+            @ApiResponse(responseCode = "401", description = "Don't Authorization", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
+    ResponseEntity<?> updateUser(@Parameter(description = "user id") Long id, @RequestBody User userUpdated);
 
-    ResponseEntity<?> removeUser(Long id);
+    @Operation(summary = "Remove User")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success!"),
+            @ApiResponse(responseCode = "404", description = "User don't found"),
+            @ApiResponse(responseCode = "401", description = "Don't Authorization", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
+    ResponseEntity<?> removeUser(@Parameter(description = "user id") Long id);
 }
