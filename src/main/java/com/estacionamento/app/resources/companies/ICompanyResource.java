922c0e51415e1424c55b813bf9edac69a9405795
João Darwin <joaodarwin.ip22@gmail.com>
2023-10-28 21:15:19 -0300

    feat: Criar documentação para resources da company
    Issue: #3

--- src/main/java/com/estacionamento/app/resources/companies/ICompanyResource.java
+++ src/main/java/com/estacionamento/app/resources/companies/ICompanyResource.java
@@ -2,20 +2,60 @@ package com.estacionamento.app.resources.companies;
 
 import com.estacionamento.app.entities.Company;
 import com.estacionamento.app.entities.dtos.responses.DataCompanyDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
+@Tag(name = "Company Resources", description = "Resources to companies")
 public interface ICompanyResource {
 
-    ResponseEntity<?> saveCompany(Company company);
+    @Operation(summary = "Save a company")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Company created success"),
+            @ApiResponse(responseCode = "401", description = "User don't authorization"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    ResponseEntity<?> saveCompany(@RequestBody Company company);
 
+    @Operation(summary = "Find all companies")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "401", description = "User don't authorization"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     ResponseEntity<List<DataCompanyDTO>> findAll();
 
-    ResponseEntity<?> findVehiclesByCompany(Long id);
+    @Operation(summary = "Find vehicles by Company")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "404", description = "Company don't found"),
+            @ApiResponse(responseCode = "401", description = "User don't authorization"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    ResponseEntity<?> findVehiclesByCompany(@Parameter(description = "Company's Id") Long id);
 
-    ResponseEntity<?> updateCompany(Long idCompany, @RequestBody Company companyUpdated);
+    @Operation(summary = "Update company")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Company updated!"),
+            @ApiResponse(responseCode = "304", description = "Company don't modified"),
+            @ApiResponse(responseCode = "404", description = "Company don't found"),
+            @ApiResponse(responseCode = "401", description = "User don't authorization"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    ResponseEntity<?> updateCompany(@Parameter(description = "Company's Id") Long idCompany, @RequestBody Company companyUpdated);
 
-    ResponseEntity<?> deleteCompany(Long id);
+    @Operation(summary = "Remove company")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Company removed"),
+            @ApiResponse(responseCode = "404", description = "Company don't found"),
+            @ApiResponse(responseCode = "401", description = "User don't authorization"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    ResponseEntity<?> deleteCompany(@Parameter(description = "Company's Id") Long id);
 }
