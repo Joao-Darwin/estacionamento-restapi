e584062189e419141ecaee5d55c05abe74ba853f
João Darwin <joaodarwin.ip22@gmail.com>
2024-04-27 22:41:14 -0300

    fix: Alterar perfil que ocorre seed do database

--- src/main/java/com/estacionamento/app/configuration/seeddatabase/SeedUserDatabase.java
+++ src/main/java/com/estacionamento/app/configuration/seeddatabase/SeedUserDatabase.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Profile("Dev")
+@Profile("dev")
 @Configuration
 public class SeedUserDatabase implements CommandLineRunner {
 
